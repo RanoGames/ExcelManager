f18a87775fa266950354e0fd69d983e10cde7d83
Max <Maksimgod5@gmail.com>
2024-11-21 17:39:29 +0500

    test

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
-import pandas as pd
-import numpy as np
+import pandas
+import numpy
+import os
 
 
-data = pd.read_excel('TestTable.xlsx', usecols='A:H')
-print(data)
+def create_empty_excel(columns: list, filename: str, sheet_name: str = 'Sheet1'):
+    df = pandas.DataFrame(columns=columns)
+
+    if not os.path.exists('excel_files'):
+        os.makedirs('excel_files')
+
+    filepath = os.path.join(filename)
+    excel_writer = pandas.ExcelWriter(filepath, engine='xlsxwriter')
+    df.to_excel(excel_writer, index=False, sheet_name=sheet_name, freeze_panes=(1, 0))
+    excel_writer._save()
+
+    return filepath
+def create_tabel_users():
+    filepath = create_empty_excel(columns=columnsList, filename=Filename+'.xlsx')
+
+columnsList = []
+Filename = input("Введите имя новой excel таблицы: ")
+Columns = int(input("Введите количество столбцов: "))
+for i in range(Columns):
+    userinput = input("ХЗ ")
+    columnsList.append(userinput)
+
+create_tabel_users()
+
+data = pandas.read_excel(f'{Filename}.xlsx', usecols=f'A:{Columns}')
